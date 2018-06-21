20d5c89519f38a942385669a73cbf304f7b02104
Viesturs Proškins <vproskins@evolutiongaming.com>
2018-06-21 22:01:51 +0300

    Code optimisation

--- youbora/youbora_auth.py
+++ youbora/youbora_auth.py
@@ -9,56 +9,38 @@ logger = logging.getLogger(__name__)
 
 
 class YouboraAuth(AuthBase):
-    def __init__(self, *args, **kwargs):
-        self.system_code = args[1]
-        self.api_key = args[0]
+    def __init__(self, api_key, system_code, **kwargs):
+        self.api_key = api_key
+        self.system_code = system_code
+        self.offset = kwargs.get("offset", 36000)
 
     def __call__(self, request):
-        self._encode(request)
+        self._prepare(request)
         return request
 
-    @staticmethod
-    def _add_date_token(request, offset=36000):
-        url = request.url
-        scheme, netloc, path, query_string, fragment = urlsplit(url)
-        query_params = parse_qsl(query_string)
-
-        future_time = int(round(time.time() * 1000) + offset)
-        query_params.append(('dateToken', future_time))
-
-        new_query_string = urlencode(query_params, doseq=True)
-        new_url = urlunsplit((scheme, netloc, path.split("?")[0], new_query_string, fragment))
-        request.url = new_url
-
-    def _add_token(self, request):
-        url = request.url
-        scheme, netloc, path, query_string, fragment = urlsplit(url)
-        query_params = parse_qsl(query_string)
-
-        m = hashlib.new("md5")
-        path = request.path_url
-        logger.debug("Signing path: {0}".format(path))
-
-        m.update(path + self.api_key)
-        token = m.hexdigest()
-        query_params.append(('token', token))
-
-        new_query_string = urlencode(query_params, doseq=True)
-        new_url = urlunsplit((scheme, netloc, path.split("?")[0], new_query_string, fragment))
-        request.url = new_url
-
     def _prepare(self, request):
-        url = request.url
-        scheme, netloc, path, query_string, fragment = urlsplit(url)
+        self.request = request
+        self.original_url = request.url
+        self.scheme, self.netloc, self.original_path, query_string, self.fragment = urlsplit(self.original_url)
+        self.query_params = parse_qsl(query_string)
+        self.path = self.original_path.replace(":system_code:", self.system_code)
 
-        new_path = path.replace(":system_code:", self.system_code)
-        print new_path
+        self._add_date_token()
+        self._add_security_token()
+        self._finalise(request)
 
-        new_url = urlunsplit((scheme, netloc, new_path, query_string, fragment))
-        request.url = new_url
+    def _add_date_token(self):
+        future_time = int(round(time.time() * 1000) + self.offset)
+        self.query_params.append(('dateToken', future_time))
 
-    def _encode(self, request):
-        self._prepare(request)
-        self._add_date_token(request, offset=3600)
-        self._add_token(request)
-        logger.debug(request.url)
+    def _add_security_token(self):
+        m = hashlib.new("md5")
+        qs_part = urlencode(self.query_params, doseq=True)
+        m.update("{0}?{1}{2}".format(self.path, qs_part, self.api_key))
+        token = m.hexdigest()
+        self.query_params.append(('token', token))
+
+    def _finalise(self, request):
+        query_string = urlencode(self.query_params, doseq=True)
+        final_url = urlunsplit((self.scheme, self.netloc, self.path.split("?")[0], query_string, self.fragment))
+        request.url = final_url
